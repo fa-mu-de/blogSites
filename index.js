8c44817b31a621cf9f102337b69af174f259d16f
fa-mu-de <73393570+fa-mu-de@users.noreply.github.com>
2020-10-24 17:50:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const admin = require("firebase-admin")
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const serviceAccount = require("./config.json")
 const path = require("path")
 
